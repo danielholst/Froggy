c53ed215771dd9a43c3b354c446fcebaa4b336df
DanielHolst <danielholst2@gmail.com>
2015-07-29 12:30:41 +0200

    Added levelselection scene and second level

---
+++ frogJumper/Assets/scripts/levelSelection.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class levelSelection : MonoBehaviour {
+
+	public int level;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (level == 1)
+			Application.LoadLevel (1);
+	}
+}
---
+++ frogJumper/Assets/scripts/mainMenuHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainMenuHandler : MonoBehaviour {
+	
+	public Texture2D icon;
+	
+	void OnGUI () {
+		if (GUI.Button (new Rect (10,10, 100, 50), icon)) {
+			print ("you clicked the icon");
+			//start game
+			Application.LoadLevel(1);
+
+		}
+		
+		if (GUI.Button (new Rect (10,70, 100, 20), "This is text")) {
+			print ("you clicked the text button");
+			Application.LoadLevel(2);
+		}
+	}
+	
+}

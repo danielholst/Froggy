bc50ec456ca739da78b288fa9f889569e59b19a1
DanielHolst <danielholst2@gmail.com>
2015-09-04 19:41:41 +0200

    Fix main menu

--- frogJumper/Assets/scripts/mainMenuHandler.cs
+++ frogJumper/Assets/scripts/mainMenuHandler.cs
@@ -3,21 +3,20 @@ using System.Collections;
 
 public class mainMenuHandler : MonoBehaviour {
 	
-	public Texture2D startButton;
-	public Texture2D levelButton;
+	private Texture2D startButton;
 	
 	void OnGUI () {
 
 		GUI.backgroundColor = Color.clear;
 
-		if (GUI.Button (new Rect (500,250, 200, 100), startButton)) {
+		if (GUI.Button (new Rect (260,300, 300, 300), startButton)) {
 			print ("Start Game");
 			//start game
 			Application.LoadLevel(2);
 
 		}
 		
-		if (GUI.Button (new Rect (500, 400, 200, 100), levelButton)) {
+		if (GUI.Button (new Rect (860, 300, 300, 300), startButton)) {
 			print ("Level Selection");
 			Application.LoadLevel(1);
 		}

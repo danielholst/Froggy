a8b59af4503ec0a96e54478b57b7dfe3d1acb83f
DanielHolst <danielholst2@gmail.com>
2015-09-10 10:10:24 +0200

    Add pause menu texture

--- frogJumper/Assets/scripts/pausMenuScript.cs
+++ frogJumper/Assets/scripts/pausMenuScript.cs
@@ -7,37 +7,49 @@ using System.Collections;
 
 public class pausMenuScript : MonoBehaviour {
 
+	public GameObject EventHandler;
 	private bool paused;
 	// Use this for initialization
 	void Start () {
 	
 		paused = false;
+
+		GetComponent<Renderer> ().enabled = false;
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKeyUp (KeyCode.Escape)) {
+		//show texture for paus menu
+		if (Input.GetKeyDown (KeyCode.Escape) && EventHandler.GetComponent<holdTime> ().startGame) {
 			print ("PAUS");
 
-			paused = true;
-		
-			//show texture for paus menu 
-
-			//add buttons for the different choices
+			if (!paused) 
+			{
+				GetComponent<Renderer> ().enabled = true;
+//				print (menuTexture.transform.position);
+				Time.timeScale = 0.0f;
+				paused = true;
+			}
+			else
+			{
+				GetComponent<Renderer> ().enabled = false;
+				Time.timeScale = 1.0f;
+				paused = false;
+			}
+		}
 
 			//if return to game is presses
-			paused = false;
+//			paused = false;
 
 			//if go back to main menu us presses
-			Application.LoadLevel (0);
-
-			//if go to level selection is presses
-//			Application.LoadLevel(1);
+//			Application.LoadLevel (0);
 
 			//if exit game is pressed
 //			Application.Quit();
 		
-		}
+
 	}
 }
